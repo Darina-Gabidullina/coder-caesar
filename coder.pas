f382e11dfa587feea79ef52f3b008a34d281835e
Darina Gabidullina <darina.gabidullina.01@bk.ru>
2016-09-25 15:56:31 +0500

    Финальная версия

--- coder.pas
+++ coder.pas
@@ -1,68 +1,51 @@
 program Coder_and_decoder;
 
-function cipher(shift1: integer; inputText1: string): string;{Зашифровка }   
+function translate(number: byte; shift: integer): byte;
+begin
+  translate := (((number - 97) + shift + 26) mod 26) + 97;
+end;
+
+function cipher(shift: integer; inputText: string): string;   
 var
   number: byte;
   i: integer;
-  symbol1: char;
+  symbol: char;
   outputSymbol: char;
-  outletText1: string;
+  outletText: string;
 begin
   i := 1;
-  for i := 1 to length(inputText1) do
+  for i := 1 to length(inputText) do
   begin
-    symbol1 := inputText1[i];
-    if ord(symbol1) = 32 then 
-    begin
+    symbol := inputText[i];
+    if symbol = ' ' then
       number := 32
-    end
     else
-      number := (((ord(symbol1) + shift1) - 18) mod 26) + 96; 
+      number := translate(ord(symbol), shift);
     outputSymbol := chr(number);
-    outletText1 := outletText1 + outputSymbol;
-    cipher := outletText1;
+    outletText := outletText + outputSymbol;
+    cipher := outletText;
   end;
 end;
 
-
-
-function decipher(shift1: integer; inputText1: string): string;{Зашифровка }   
-var
-  number: byte;
-  i: integer;
-  symbol1: char;
-  outputSymbol: char;
-  outletText1: string;
-begin
-  i := 1;
-  for i := 1 to length(inputText1) do
-  begin
-    symbol1 := inputText1[i];
-    if ord(symbol1) = 32 then number := 32
-    else
-      number := (((ord(symbol1) - shift1) - 19) mod 26) + 97; 
-    outputSymbol := chr(number);
-    outletText1 := outletText1 + outputSymbol;
-    decipher := outletText1;
-  end;
-end;
-
-
+procedure main();
 var
   choose: integer;
   shift: integer;
   inputText, outletText: string;
-
 begin
-  writeln('Напишите кол-во смещений. ');
+  writeln('РќР°РїРёС€РёС‚Рµ РєРѕР»-РІРѕ СЃРјРµС‰РµРЅРёР№. ');
   readln(shift);
-  writeln('Напишите текст. Пробелы будут заменятся другими символами.');
+  writeln('РќР°РїРёС€РёС‚Рµ С‚РµРєСЃС‚. РџСЂРѕР±РµР»С‹ Р±СѓРґСѓС‚ Р·Р°РјРµРЅСЏС‚СЃСЏ РґСЂСѓРіРёРјРё СЃРёРјРІРѕР»Р°РјРё.');
   readln(inputText);
-  writeln('Зашифровать-1, Дешифровать-2.Только англ. алфавит.');
+  writeln('Р—Р°С€РёС„СЂРѕРІР°С‚СЊ-1, Р”РµС€РёС„СЂРѕРІР°С‚СЊ-2.РўРѕР»СЊРєРѕ Р°РЅРіР». Р°Р»С„Р°РІРёС‚.');
   readln(choose);
   if choose < 2 then
     outletText := cipher(shift, inputText)
   else
-    outletText := decipher(shift, inputText );
+    outletText := cipher(-shift, inputText);
   writeln(outletText);
+end;
+
+begin
+  main();
 end.
